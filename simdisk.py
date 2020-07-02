563c637db5886a2f0194544d7ccd0a4188e181a3
PHIN1GHT <kale1d0@qq.com>
2020-07-02 14:46:04 +0800

    dix dir

--- simdisk.py
+++ simdisk.py
@@ -1,13 +1,14 @@
-import os, math
+import os, math, struct
 import numpy as np
 
 class Bitmap(object):
     def __init__(self, n):
         num = n/32 if n%32==0 else math.ceil(n/32)
-        self.map = np.zeros(int(num),dtype=np.uint32)
+        self._map = np.zeros(int(num),dtype=np.uint32)
         self._next_pos = 0
         self._total = n
         self._used = 0
+        self._size = int(num * 4) # bytes
 
     def _tran_pos(self, n):
         sector = int(n/32 if n%32 == 0 else math.floor(n/32))
@@ -16,21 +17,21 @@ class Bitmap(object):
 
     def set(self,pos,value=True):
         sector, offset = self._tran_pos(pos)
-        ori_val = self.map[sector]
+        ori_val = self._map[sector]
         if value:
-            new_val = self.map[sector] | (1<<offset)
+            new_val = self._map[sector] | (1<<offset)
         else:
-            new_val = self.map[sector] & (~(1<<offset))
+            new_val = self._map[sector] & (~(1<<offset))
         if not ori_val == new_val:
             self._used += 1 if value else -1
-        self.map[sector] = new_val
+        self._map[sector] = new_val
 
     def flip(self,pos):
         self.set(pos, not self.get(pos))
 
     def get(self,pos):
         sector, offset = self._tran_pos(pos)
-        return self.map[sector] >> offset & 1
+        return self._map[sector] >> offset & 1
 
     def next(self):
         if not self._used < self._total:
@@ -42,26 +43,114 @@ class Bitmap(object):
         return self._next_pos
 
 class Superblock(object):
-    inode_map_size = ''
-    inode_map = ''
-    inode_region_pos = ''
-    inode_struct_size = ''
-
-    block_map_size = ''
-    block_map = ''
-    block_region_idx = ''
-    block_struct_size = ''
-
-    dir_region_pos = ''
-    
     def __init__(self,
         inode_num=102400,
         block_num=102400,
         inode_struct_size=32,
-        block_struct_size=1024):
-        self.inode_map = Bitmap(inode_num)
-        self.block_map = Bitmap(block_num)
-        pass
+        block_struct_size=1024,
+        dir_region_pos = 32*1024):
+        self._inode_num = inode_num
+        self._block_num = block_num
+        self._inode_struct_size = inode_struct_size
+        self._block_struct_size = block_struct_size
+        self._inode_map = Bitmap(inode_num)
+        self._block_map = Bitmap(block_num)
+        #self._size = self._inode_map._size + self._block_map._size + 7 * 4
+        self._size = 32 * 1024
+        self._dir_region_pos = self._size
+        self._inode_region_pos = self._dir_region_pos + 4096 * 1024
+        self._block_region_pos = self._inode_region_pos + 1024 * 1024
+    
+    def encode_into(self,btarr,offset=0):
+        struct.pack_into('I',btarr,offset,self._inode_map._size)
+        offset += 4
+        for v in self._inode_map._map:
+            struct.pack_into('I',btarr,offset,v)
+            offset += 4
+        struct.pack_into('I',btarr,offset,self._inode_region_pos)
+        offset += 4
+        struct.pack_into('I',btarr,offset,self._inode_struct_size)
+        offset += 4
+
+        struct.pack_into('I',btarr,offset,self._block_map._size)
+        offset += 4
+        for v in self._block_map._map:
+            struct.pack_into('I',btarr,offset,v)
+            offset += 4
+        struct.pack_into('I',btarr,offset,self._block_region_pos)
+        offset += 4
+        struct.pack_into('I',btarr,offset,self._block_struct_size)
+        offset += 4
+
+        struct.pack_into('I',btarr,offset,self._dir_region_pos)
+        offset += 4
+        return offset
+
+    @classmethod
+    def decode_from(cls,btarr,offset=0):
+        blk = Superblock()
+        map_size = struct.unpack_from('I',btarr,offset)[0] >> 2
+        offset += 4
+        for i in range(map_size):
+            blk._inode_map._map[i] = struct.unpack_from('I',btarr,offset)[0]
+            offset += 4
+        blk._inode_region_pos = struct.unpack_from('I',btarr,offset)[0]
+        offset += 4
+        blk._inode_struct_size = struct.unpack_from('I',btarr,offset)[0]
+        offset += 4
+        
+        map_size = struct.unpack_from('I',btarr,offset)[0] >> 2
+        offset += 4
+        for i in range(map_size):
+            blk._block_map._map[i] = struct.unpack_from('I',btarr,offset)[0]
+            offset += 4
+        blk._block_region_pos = struct.unpack_from('I',btarr,offset)[0]
+        offset += 4
+        blk._block_struct_size = struct.unpack_from('I',btarr,offset)[0]
+        offset += 4
+        
+        blk._dir_region_pos = struct.unpack_from('I',btarr,offset)[0]
+        offset += 4
+
+        return blk
+
+
+class Dir(object):
+    def __init__(self, name="/", inode=0):
+        self._name = name
+        self._inode = inode
+        self._list = []
+
+    def encode_into(self,btarr,offset=0):
+        struct.pack_into('32s',btarr,offset,self._name.encode('utf-8'))
+        offset += 32
+        struct.pack_into('I',btarr,offset,self._inode)
+        offset += 4
+        struct.pack_into('I',btarr,offset,len(self._list))
+        offset += 4
+        for i in self._list:
+            struct.pack_into('32s',btarr,offset,i['name'].encode('utf-8'))
+            offset += 32
+            struct.pack_into('I',btarr,offset,i['inode'])
+            offset += 4
+        return offset
+    
+    @classmethod
+    def decode_from(cls,btarr,offset=0):
+        name = struct.unpack_from('32s',btarr,offset)[0].decode('utf-8').strip(b'\x00'.decode())
+        offset += 32
+        inode = struct.unpack_from('I',btarr,offset)[0]
+        offset += 4
+        d = Dir(name, inode)
+        size = struct.unpack_from('I',btarr,offset)[0]
+        offset += 4
+        for i in range(size):
+            name = struct.unpack_from('32s',btarr,offset)[0].decode('utf-8').strip(b'\x00'.decode())
+            offset += 32
+            inode = struct.unpack_from('I',btarr,offset)[0]
+            offset += 4
+            d._list.append({"name":name,'inode':inode})
+        return d
 
 class INode(object):
     premcode = ""
@@ -90,12 +179,20 @@ func['logout'] = logout
 
 buffer = b''
 
+class FileSystem(object):
+    def __init__(self,buffer=None):
+        if buffer:
+            self._super_block = Superblock.decode_from(buffer)
+
+
+fsys = FileSystem()
+
 def init():
     if os.path.exists('diskfile'):
         with open('diskfile', 'rb') as df:
             buffer = bytearray(df.read())
     else:
-        buffer = bytearray(100 * 1024 * 1024) # 100MB = 100 * 1024 * 1024 B
+        buffer = bytearray(100 * 1024 * 1024) # 100MB = 100 * 1024 * 
         with open('diskfile', 'wb') as df:
             df.write(buffer)
 
